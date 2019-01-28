d81bfaab8c5b54c84434ce531ef6af6332112a1f
ad05bzag <b.zagidullin@gmail.com>
2019-01-28 20:02:30 +0200

    init end-to-end analysis of sd of css values

---
+++ filtering.py
@@ -0,0 +1,95 @@
+import pandas as pd
+from tqdm import tqdm
+import pickle
+
+'''
+This script is used to calculate the sd and cv of css values for drugA/drugB/cell_line combinations.
+First separation is into combinations present in single_study vs multiple_study. These are first level dict keys
+
+
+A second line of analysis is comparison of sd of css values present in multiple studies for each of the 
+drugA/drugB/cell_line combos vs all drugA/drugB combos
+'''
+
+#%% getting input and init'ing variables
+input_all = pd.read_csv('/Users/zagidull/Documents/fimm_files/chemical_similarity/classifier/summary_css_dss_ic50_synergy_smiles_study.csv', sep = ';')
+
+#%% running std analysis on input_all
+
+grouped = input_all.groupby(['drug_row_id', 'drug_col_id', 'cell_line_id'])
+
+
+#%% function for getting sd and cv of dataframe
+def calc(dataframe):
+    """
+    :param dataframe with at least the following columns ['drug_row_id', 'drug_col_id', 'cell_line_id', 'study', 'css']
+    :return: standard deviation as float, coefficient of variation as float, both rounded to 4 digits and study as set
+    """
+    sd = round(dataframe['css'].std(), ndigits=4)
+    cv = round(sd/dataframe['css'].mean(), ndigits=4)
+    study = set(dataframe['study'])
+    return sd, cv, study
+
+
+#%%
+timer = len(grouped)
+with tqdm(total=timer) as pbar:
+    key_holder = {}
+    for key, group in grouped:
+        row = key[0]
+        col = key[1]
+        cell = key[2]
+        reversed_key = (col, row, cell)
+        if key in key_holder:  # we are checking if this combo has already been tested, if yes: pass
+            pass
+        else:
+            if reversed_key in key_holder:  # we are checking if exchanging drug_row and drug_col ID's has already
+                                            # been tested for, as these combos would be identical.
+                                            # If yes: pass
+                pass
+            else:
+                df = input_all.loc[(input_all.drug_row_id == col) & (input_all.drug_col_id == row) & (
+                            input_all.cell_line_id == cell)]  # exchanging drug_row and drug_col with each other
+                if not df.empty:  # if NOT empty, then exchanging resulted in additional data
+                    group = group.append(df)
+                    sd_out, cv_out, study_out = calc(group)
+                    if len(study_out) == 1:
+                        key_holder[key] = {'type': 'single_study', 'study': study_out, 'sd': sd_out, 'cv': cv_out}
+
+                    elif len(study_out) > 1:
+                        key_holder[key] = {'type': 'multiple_study', 'study': study_out, 'sd_cell_line': sd_out,
+                                           'cv_cell_line': cv_out, 'study_wide': {}}
+                        for study in study_out:  # if multiple studies, test drug_row and drug_col in each study
+                                                 # where this combo was found across all the cell lines
+                            df = input_all.loc[(input_all.drug_row_id == row) & (input_all.drug_col_id == col) & (
+                                        input_all.study == study)]
+                            df1 = input_all.loc[(input_all.drug_row_id == col) & (input_all.drug_col_id == row) & (
+                                        input_all.study == study)]
+                            df = df.append(df1)
+                            sd_out_all, cv_out_all, _ = calc(df)  # we don't need study returned, as we iterate by study
+                            key_holder[key]['study_wide'][study] = {'sd_study_wide': sd_out_all, 'cv_study_wide': cv_out_all}
+
+                else:
+                    if len(group.index) < 2:  # we must have more than one row to calculate sd and cv. Check for that
+                        pass
+                    else:
+                        sd_out, cv_out, study_out = calc(group)
+                        if len(study_out) == 1:
+                            key_holder[key] = {'type': 'single_study', 'study': study_out, 'sd': sd_out, 'cv': cv_out}
+
+                        elif len(study_out) > 1:
+                            key_holder[key] = {'type': 'multiple_study', 'study': study_out, 'sd_cell_line': sd_out,
+                                               'cv_cell_line': cv_out, 'study_wide': {}}
+                            for study in study_out:
+                                df = input_all.loc[(input_all.drug_row_id == row) & (input_all.drug_col_id == col) & (
+                                            input_all.study == study)]
+                                df1 = input_all.loc[(input_all.drug_row_id == col) & (input_all.drug_col_id == row) & (
+                                            input_all.study == study)]
+                                df = df.append(df1)
+                                sd_out_all, cv_out_all, _ = calc(df)
+                                key_holder[key]['study_wide'][study] = {'sd_study_wide': sd_out_all, 'cv_study_wide': cv_out_all}
+        pbar.update(1)
+
+# saving on disk
+with open('filter_input.pickle', 'wb') as f:
+    pickle.dump(key_holder, f, pickle.HIGHEST_PROTOCOL)
---
+++ viz.py
@@ -0,0 +1,110 @@
+#%% initial import
+import numpy as np
+from scipy.stats import ttest_ind
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+import pickle
+
+sns.set(style="seaborn-colorblind")
+sns.set(rc={'figure.figsize': (15.7, 7.27)})
+
+#with open('filter_input.pickle', 'wb') as f:
+#    pickle.dump(key_holder, f, pickle.HIGHEST_PROTOCOL)
+
+with open('/Users/zagidull/PycharmProjects/test_scientific/filter_input.pickle', 'rb') as f:
+    data = pickle.load(f)
+#%% unpacking the dict to separate into single or multiple studies
+key_holder = data
+counter = 0
+single_sd = []
+multiple_sd = []
+al = []
+on = []
+keys = []
+for key, value in key_holder.items():
+    if value['type'] == 'single_study':
+        single_sd.append(value['sd'])
+    else:
+        keys.append(key)
+        multiple_sd.append(value['sd_cell_line'])
+        al.append(value['study_wide']['ALMANAC']['sd_study_wide'])
+        on.append(value['study_wide']['ONEIL']['sd_study_wide'])
+
+# we have to use list(set()), since there is a lot of repeated elements, as we are searching several times for the same drug combo.
+# It happens because we are removing the cell_line part, and using study instead. Which results in that behavoir
+al = list(set(al))
+on = list(set(on))
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+sns.distplot(single_sd, hist=False, kde=True, rug=False, kde_kws={'linewidth': 2},
+             hist_kws={"histtype": "step", 'linewidth': 1, "alpha": 1}, bins='auto', color='blue',
+             label='Single' + ' number of datapoints: ' + str(len(single_sd)))
+sns.distplot(multiple_sd, hist=False, kde=True, rug=False, kde_kws={'linewidth': 2},
+             hist_kws={"histtype": "step", 'linewidth': 1, "alpha": 1}, bins='auto', color='red',
+             label='multiple' + ' number of datapoints: ' + str(len(multiple_sd)))
+sns.distplot(al, hist=False, kde=True, rug=False, kde_kws={'linewidth': 2},
+             hist_kws={"histtype": "step", 'linewidth': 1, "alpha": 1}, bins='auto', color='green',
+             label='ALMANAC' + ' number of datapoints: ' + str(len(al)))
+sns.distplot(on, hist=False, kde=True, rug=False, kde_kws={'linewidth': 2},
+             hist_kws={"histtype": "step", 'linewidth': 1, "alpha": 1}, bins='auto', color='yellow',
+             label='ONEIL' + ' number of datapoints: ' + str(len(on)))
+
+plt.title('sd of CSS values')
+plt.ylabel('Density')
+plt.xlabel('sd')
+to_save = plt.gcf()
+plt.show()
+to_save.savefig('sd of CSS values.png')
+#%%  just picking up examples that are present in multiple studies and sorting them from highest to lowest
+for_sorting = {}
+for key, value in key_holder.items():
+    if value['type'] == 'multiple_study':
+        for_sorting[key] = value['sd_cell_line']
+out = sorted(for_sorting.items(), key=lambda kv: kv[1], reverse=True)
+out = out[:150]
+#%%
+#single_sd = []
+#multiple_sd = []
+#al = {}
+#on = {}
+#keys = []
+#for key, value in key_holder.items():
+#    if value['type'] == 'single_study':
+#        single_sd.append(value['sd'])
+#    else:
+#        keys.append(key)
+#        multiple_sd.append(value['sd_cell_line'])
+#       al[key] = value['study_wide']['ALMANAC']['sd_study_wide']
+#       on[key] = value['study_wide']['ONEIL']['sd_study_wide']
+
+
+#%% drawing pretty pictures
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1)
+#
+# sns.distplot(al, hist=False, kde=True, rug=False, kde_kws={'linewidth': 2},
+#              hist_kws={"histtype": "step", 'linewidth': 1, "alpha": 1},
+#              bins='auto', color='blue', label='ALMANAC' + ' number of datapoints: ' + str(len(al)))
+# sns.distplot(on, hist=False, kde=True, rug=False, kde_kws={'linewidth': 2},
+#              hist_kws={"histtype": "step", 'linewidth': 1, "alpha": 1},
+#              bins='auto', color='darkred', label='ONEIL' + ' number of datapoints: ' + str(len(on)))
+#
+# props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+# textstr = '\n'.join((
+#     'ALMANAC = multiples only from NCI across all cell_lines',
+#     'ONEIL = multiples only from O\'Neil across all cell_lines'
+# ))
+#
+# plt.title('Standard deviation of CSS values.')
+# # ax.text(44.5, .125,'bipositional = reversing drug row and drug col finds occurences', fontsize=9) #add text
+# # ax.text(44.5, .132,'unipositional = reversing drug row and drug col finds NO occurences', fontsize=9) #add text
+# # ax.text(44.5, .146,'singles = present in one study', fontsize=9) #add text
+# ax.text(.712, .75, textstr, fontsize=10, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes,
+#         bbox=props)  # add text
+#
+# plt.ylabel('Density')
+# plt.xlabel('std')
+# plt.show()
+
+#%%
